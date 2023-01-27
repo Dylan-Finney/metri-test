56789b8f597e6513e7b716e48cd7d8e1f573594e
Dylan Finney <dylan.finney@outlook.com>
2023-01-27 14:37:34 +0000

    Style Changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./App.css"
 
 function App() {
   const [userIDStore, setUserIDStore] = useState("")
   const [startDate, setStartDate] = useState(new Date());
   const [startDateString, setStartDateString] = useState(`${startDate.getFullYear()}-${('0' + (startDate.getMonth()+1)).slice(-2)}-${('0' + startDate.getDate()).slice(-2)}`);
-  const [customUserID, setCustomUserID] = useState("test-account-2")
+  const [customUserID, setCustomUserID] = useState("dylan-finney")
   const [provider, setProvider] = useState("")
 
   const setUpMetriport = async () => {
@@ -95,51 +96,113 @@ function App() {
     console.log(user)
   }
   const revokeProvider = async () => {
-    console.log("test")
     const revoke = await axios.delete(`https://api.sandbox.metriport.com/user/revoke?userId=${userIDStore}&provider=${provider}`,{
       headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
     })
     console.log(revoke)
   }
+  const getAllData = async () => {
+    const userID = await axios({
+      method: "post",
+      url: `https://api.sandbox.metriport.com/user?appUserId=${customUserID}`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    var userIDStore = userID.data.userId
+    const activity = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/activity?userId=${userIDStore}&date=${startDateString}`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(activity)
+    const biometrics = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/biometrics?userId=${userIDStore}&date=${startDateString}`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(biometrics)
+    const body = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/body?userId=${userIDStore}&date=${startDateString}`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(body)
+    const nutrition = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/nutrition?userId=${userIDStore}&date=2023-01-25`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(nutrition)
+    const sleep = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/sleep?userId=${userIDStore}&date=2023-01-25`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(sleep)
+    const user = await axios({
+      method: "get",
+      url: `https://api.sandbox.metriport.com/user?userId=${userIDStore}&date=2023-01-25`,
+      headers: { 'x-api-key': process.env.REACT_APP_SECRET_KEY }
+    })
+    console.log(user)
+    console.log({
+      "activity": activity.data,
+      "biometrics": biometrics.data,
+      "body": body.data,
+      "nutrition": nutrition.data,
+      "sleep": sleep.data,
+      "user": user.data,
+    })
+  }
   useEffect(()=>{
     setStartDateString(`${startDate.getFullYear()}-${('0' + (startDate.getMonth()+1)).slice(-2)}-${('0' + startDate.getDate()).slice(-2)}`)
   },[startDate])
+  const rootStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  }
   return (
-    <div className="App">
+    <div className="App" style={rootStyle}>
         Username:<input value={customUserID} onChange={(event)=>{
           if (event.target.value[event.target.value.length - 1] !== " "){
             // setCustomUserID(event.target.value)
           }
           }}></input>
-          <br/>
-        Date:
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+
+        Date you want data retrived from:
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} style={{width: "unset"}}/>
         
         <button onClick={setUpMetriport}>Set Up</button>
-        <br/>
+
         <button onClick={() => {getUserID(false)}}>Get User ID</button>
-        <br/>
-        <select value={provider} onChange={e=>setProvider(e.target.value)}>
-          <option value={"apple"}>Apple</option>
-          <option value={"cronometer"}>Cronometer</option>
-          <option value={"fitbit"}>Fitbit</option>
-          <option value={"oura"}>Oura</option>
-          <option value={"whoop"}>Whoop</option>
-          <option value={"withings"}>Withings</option>
-        </select> <button onClick={revokeProvider}>Revoke</button>
-        <br/>
+        <div style={{
+          display: "flex",
+          flexDirection: "row"
+        }}>
+          <select value={provider} onChange={e=>setProvider(e.target.value)}>
+            <option value={"apple"}>Apple</option>
+            <option value={"cronometer"}>Cronometer</option>
+            <option value={"fitbit"}>Fitbit</option>
+            <option value={"oura"}>Oura</option>
+            <option value={"whoop"}>Whoop</option>
+            <option value={"withings"}>Withings</option>
+          </select> <button onClick={revokeProvider}>Revoke</button>
+        </div>
+        
+
         <button onClick={getActivityData}>Get Activity Data</button>
-        <br/>
+
         <button onClick={getBiometricsData}>Get Biometrics Data</button>
-        <br/>
+
         <button onClick={getBodyData}>Get Body Data</button>
-        <br/>
+
         <button onClick={getNutritionData}>Get Nutrition Data</button>
-        <br/>
+
         <button onClick={getSleepData}>Get Sleep Data</button>
-        <br/>
+
         <button onClick={getUserData}>Get User Data</button>
-        <br/>
+        <button onClick={getAllData}>Get All Data</button>
         See Console for results
       {/* </header> */}
     </div>
